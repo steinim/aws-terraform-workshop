8ffd316ca4f19b9ac40fe3ac805a6da4f91d84cb
Stein Inge Morisbak <steinim@gmail.com>
2018-04-10 10:36:29 +0200

    query for subnet and security group ids

--- create.sh
+++ create.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 env=${1}
-private_subnet_ids=
-public_subnet_ids=
-security_group_ids=
+vpc_id=$(envchain aws2 aws ec2 describe-vpcs --filters "Name=tag:Name,Values=${env}_vpc" --query 'Vpcs[*].[VpcId]' --output text)
+private_subnet_ids=$(envchain aws2 aws ec2 describe-subnets --filters "Name=vpc-id,Values=${vpc_id},Name=tag:Name,Values=${env}_private_subnet*" --query 'Subnets[*].[SubnetId]' --output text)
+public_subnet_ids=$(envchain aws2 aws ec2 describe-subnets --filters "Name=vpc-id,Values=${vpc_id},Name=tag:Name,Values=${env}_public_subnet*" --query 'Subnets[*].[SubnetId]' --output text)
+security_group_ids=$(envchain aws2 aws ec2 describe-security-groups --filters "Name=group-name,Values=${env}_hello_app_sg" --query 'SecurityGroups[*].[GroupId]' --output text)
 
 # remove files on script exit
 trap 'rm -f app.zip' EXIT
@@ -19,6 +20,8 @@ Variables:
 Press ENTER to continue
 EOF
 
+read
+
 ./package.sh ${env}
 
 envchain aws2 eb create ${env}-helloworld \
@@ -28,7 +31,7 @@ envchain aws2 eb create ${env}-helloworld \
   --vpc.ec2subnets ${private_subnet_ids}  \
   --vpc.elbpublic \
   --vpc.elbsubnets ${public_subnet_ids} \
-  --vpc.securitygroups ${security_group_id} \
+  --vpc.securitygroups ${security_group_ids} \
   --instance_type t2.micro \
   --platform java-8 \
   --cname ${env}-helloworld \
